79ef446e38c1b4d149df04eca3e24bcb5d6c243c
KuaqSon <qson.ng@gmail.com>
2020-11-27 01:05:04 +0700

    Commit with ❤️

--- Readme.md
+++ Readme.md
@@ -1,3 +1,20 @@
 # Doro Focus app
 
 Simple Pomodoro clock running on Mac-os status bar
+
+
+## Build app 
+
+### 1. Install packages
+
+```
+pip install -r requirements.txt
+
+```
+
+### 2. Run Build cmd
+
+```
+python setup.py py2app -A
+
+```
--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
 from setuptools import setup
 import pkg_resources.py2_warn
 
-APP = ['pomodoro.py']
+APP = ["pomodoro.py"]
 DATA_FILES = []
 OPTIONS = {
-    'argv_emulation': True,
-    'iconfile': 'icon.icns',
-    'plist': {
-        'CFBundleShortVersionString': '0.2.0',
-        'LSUIElement': True,
+    "argv_emulation": True,
+    "iconfile": "icon.icns",
+    "plist": {
+        "CFBundleShortVersionString": "0.2.0",
+        "LSUIElement": True,
     },
-    'packages': ['rumps'],
+    "packages": ["rumps"],
+    "excludes": ["pillow", "Image"],  # exclude unwanted dependencies
 }
 
 setup(
     app=APP,
-    name='Doro Focus',
+    name="DoroFocus",
     data_files=DATA_FILES,
-    options={'py2app': OPTIONS},
-    setup_requires=['py2app'], install_requires=['rumps']
+    options={"py2app": OPTIONS},
+    setup_requires=["py2app"],
+    install_requires=["rumps"],
 )
